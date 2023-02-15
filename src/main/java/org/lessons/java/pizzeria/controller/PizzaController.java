2b55706382c2293881e8ff0b40283aaa288d5496
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-15 15:55:13 +0100

    modificato index per la ricerca di una pizza

--- src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -19,8 +20,14 @@ public class PizzaController {
 	private PizzaRepository pizzaRepository;
 
 	@GetMapping
-	public String index(Model model) {
-		List<Pizza> pizzaList = pizzaRepository.findAll();
+	public String index(@RequestParam(name = "pizza", required = false) String keyword, Model model) {
+		List<Pizza> pizzaList;
+
+		if (keyword == null || keyword.isEmpty())
+			pizzaList = pizzaRepository.findAll();
+		else
+			pizzaList = pizzaRepository.findByNameLike("%" + keyword + "%");
+
 		model.addAttribute("pizzas", pizzaList);
 
 		return "pizzas/index";
