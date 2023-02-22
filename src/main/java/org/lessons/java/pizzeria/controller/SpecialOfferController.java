a02fcca3ee87bdcb28e9f8debd6c4b400616fdf6
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-22 15:17:55 +0100

    definizione metodi per la creazione di un'offerta + nuova route

--- src/main/java/org/lessons/java/pizzeria/controller/SpecialOfferController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/SpecialOfferController.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/special-offer")
+@RequestMapping("/special-offers")
 public class SpecialOfferController {
 
 	@Autowired
@@ -23,6 +24,21 @@ public class SpecialOfferController {
 
 		model.addAttribute("specialOffer", specialOffers);
 		return "specialOffers/index";
+	}
+
+	@GetMapping("/create")
+	public String create(Model model) {
+		SpecialOffer specialOffer = new SpecialOffer();
+		model.addAttribute("specialOffer", specialOffer);
+
+		return "specialOffers/create";
+	}
+
+	@PostMapping("/create")
+	public String store(SpecialOffer formOffer, Model model) {
+		offerRepo.save(formOffer);
+
+		return "redirect:/";
 	}
 
 }
