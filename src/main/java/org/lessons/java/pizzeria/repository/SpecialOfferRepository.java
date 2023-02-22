b48103eca312022c48d2b4d1e8f06ed2e268bc89
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-22 14:38:37 +0100

    Creazione SpecialOfferRepository

---
+++ src/main/java/org/lessons/java/pizzeria/repository/SpecialOfferRepository.java
@@ -0,0 +1,8 @@
+package org.lessons.java.pizzeria.repository;
+
+import org.lessons.java.pizzeria.model.SpecialOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecialOfferRepository extends JpaRepository<SpecialOffer, Long> {
+
+}
