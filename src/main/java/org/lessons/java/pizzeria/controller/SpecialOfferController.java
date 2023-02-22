164bddc6f6e20fd0142bbd5414f7d09af48c19fe
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-22 15:53:05 +0100

    cambio nome attributo del model da specialOffer a specialOffers

--- src/main/java/org/lessons/java/pizzeria/controller/SpecialOfferController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/SpecialOfferController.java
@@ -2,7 +2,9 @@ package org.lessons.java.pizzeria.controller;
 
 import java.util.List;
 
+import org.lessons.java.pizzeria.model.Pizza;
 import org.lessons.java.pizzeria.model.SpecialOffer;
+import org.lessons.java.pizzeria.repository.PizzaRepository;
 import org.lessons.java.pizzeria.repository.SpecialOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,12 +14,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/special-offers")
 public class SpecialOfferController {
+
+	@Autowired
+	PizzaRepository pizzaRepo;
 
 	@Autowired
 	SpecialOfferRepository offerRepo;
@@ -26,13 +33,22 @@ public class SpecialOfferController {
 	public String index(Model model) {
 		List<SpecialOffer> specialOffers = offerRepo.findAll();
 
-		model.addAttribute("specialOffer", specialOffers);
+		model.addAttribute("specialOffers", specialOffers);
 		return "specialOffers/index";
 	}
 
 	@GetMapping("/create")
-	public String create(Model model) {
+	public String create(@RequestParam(name = "pizzaId", required = true) Integer pizzaId, Model model)
+			throws Exception {
 		SpecialOffer specialOffer = new SpecialOffer();
+
+		try {
+			Pizza pizza = pizzaRepo.getReferenceById(pizzaId);
+			specialOffer.setPizza(pizza);
+		} catch (EntityNotFoundException e) {
+			throw new Exception("Pizza not present. Id=" + pizzaId);
+		}
+
 		model.addAttribute("specialOffer", specialOffer);
 
 		return "specialOffers/create";
