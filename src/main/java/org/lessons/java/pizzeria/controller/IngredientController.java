17648e368a2672b76eb947819a8256ee687cf137
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-23 15:30:05 +0100

    aggiunto edit ed update IngredientController

--- src/main/java/org/lessons/java/pizzeria/controller/IngredientController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/IngredientController.java
@@ -7,9 +7,14 @@ import org.lessons.java.pizzeria.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -35,7 +40,31 @@ public class IngredientController {
 	}
 
 	@PostMapping("/create")
-	public String store(Ingredient formIngredient, Model model) {
+	public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+			Model model) {
+
+		if (bindingResult.hasErrors())
+			return "ingredients/create";
+
+		ingredientRepository.save(formIngredient);
+
+		return "redirect:/ingredients";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		Ingredient ingredient = ingredientRepository.getReferenceById(id);
+		model.addAttribute("ingredient", ingredient);
+
+		return "ingredients/edit";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+			Model model) {
+
+		if (bindingResult.hasErrors())
+			return "ingredients/edit";
 
 		ingredientRepository.save(formIngredient);
 
