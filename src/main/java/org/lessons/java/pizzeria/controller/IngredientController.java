cc560f9842dc1cf74e856117ee173df8dedd314f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-23 14:38:36 +0100

    aggiunte rotte per la creazione di Ingredient

--- src/main/java/org/lessons/java/pizzeria/controller/IngredientController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,11 +19,27 @@ public class IngredientController {
 	private IngredientRepository ingredientRepository;
 
 	@GetMapping
-	private String index(Model model) {
+	public String index(Model model) {
 		List<Ingredient> ingredients = ingredientRepository.findAll();
 		model.addAttribute("ingredients", ingredients);
 
 		return "ingredients/index";
+	}
+
+	@GetMapping("/create")
+	public String create(Model model) {
+		Ingredient ingredient = new Ingredient();
+		model.addAttribute("ingredient", ingredient);
+
+		return "ingredients/create";
+	}
+
+	@PostMapping("/create")
+	public String store(Ingredient formIngredient, Model model) {
+
+		ingredientRepository.save(formIngredient);
+
+		return "redirect:/ingredients";
 	}
 
 }
