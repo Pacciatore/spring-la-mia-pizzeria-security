4942e9e0e332674dd36d718d766e968c0b2e79f3
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-24 15:01:06 +0100

    aggiunta api per dettaglio pizza

--- src/main/java/org/lessons/java/pizzeria/api/ApiPizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/api/ApiPizzaController.java
@@ -1,12 +1,16 @@
 package org.lessons.java.pizzeria.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.pizzeria.model.Pizza;
 import org.lessons.java.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +25,20 @@ public class ApiPizzaController {
 
 	@GetMapping
 	public List<Pizza> index(@RequestParam(name = "pizza", required = false) String keyword) {
-
 		if (keyword == null || keyword.isEmpty())
 			return pizzaRepo.findAll();
 		else
 			return pizzaRepo.findByNameLike("%" + keyword + "%");
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<Pizza> show(@PathVariable(name = "id") Integer id) {
+		Optional<Pizza> result = pizzaRepo.findById(id);
+
+		if (result.isPresent())
+			return new ResponseEntity<Pizza>(result.get(), HttpStatus.OK);
+		else
+			return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
 
 	}
 
