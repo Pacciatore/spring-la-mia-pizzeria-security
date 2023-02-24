1d82c55be0c2969b4105e001f8f229a7789bb19d
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-24 15:41:33 +0100

    aggiunta rotta api per la creazione

--- src/main/java/org/lessons/java/pizzeria/api/ApiPizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/api/ApiPizzaController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +41,11 @@ public class ApiPizzaController {
 			return new ResponseEntity<Pizza>(result.get(), HttpStatus.OK);
 		else
 			return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
+	}
 
+	@PostMapping()
+	public Pizza create(@RequestBody Pizza pizza) {
+		return pizzaRepo.save(pizza);
 	}
 
 }
