f0e4b037e9a9f441c528b0c15a85293e6afe7db0
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-27 15:44:35 +0100

    aggiunti ingredienti nella tabella dell'homepage

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -12,14 +12,27 @@ function pizzasList() {
             document.querySelector('#pizzas_table').innerHTML = '';
 
             pizzasList.forEach(pizza => {
+
+                const ingredients = pizza.ingredients;
+
+                console.log('expected ingredients', ingredients);
+
                 document.querySelector('#pizzas_table').innerHTML += `
                 <tr>
                     <td><a href="./pizza?id=${pizza.id}"> ${pizza.id} </a></td>
                     <td>${pizza.name}</td>
                     <td>${pizza.description}</td>
-                    <td>Vedere come implementare gli ingredienti!</td>
+                    <td><ul id="ingredients-list-${pizza.id}"></ul></td>
                     <td>${pizza.price}€</td>
                 </tr>`;
+
+                ingredients.forEach(ingredient => {
+                    document.querySelector(`#ingredients-list-${pizza.id}`).innerHTML += `
+                    <li>${ingredient.name}</li>
+                    `;
+                })
+
+
             });
 
         }).catch((result) => {
