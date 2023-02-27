ed0d92a326bac41568acd7adbdbb346e1b397bca
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-27 15:37:17 +0100

    aggiunto show html e js per il dettaglio della pizza

---
+++ src/main/resources/static/js/show.js
@@ -0,0 +1,35 @@
+console.log('JS OK!');
+
+const URLParams = new URLSearchParams(window.location.search);
+const pizzaId = URLParams.get('id');
+
+pizzaDetail();
+
+function pizzaDetail() {
+    axios.get(`http://localhost:8080/api/pizzas/${pizzaId}`)
+        .then((result) => {
+
+            const pizza = result.data;
+            console.log('Richiesta ok', pizza);
+
+            document.querySelector('#pizza-name').innerHTML = `${pizza.name}`;
+
+            document.querySelector('#pizza-img').src = `${pizza.imgUrl}`;
+            document.querySelector('#pizza-img').alt = `${pizza.name}`;
+
+            document.querySelector('#pizza-description').innerHTML = `${pizza.description}`;
+
+            pizza.ingredients.forEach(ingredient => {
+                document.querySelector('#pizza-ingredients').innerHTML += `
+                <li>${ingredient.name}</li>
+                `;
+            });
+
+
+            document.querySelector('#pizza-price').innerHTML = `${pizza.price}€`;
+
+        }).catch((result) => {
+            console.error('Errore nella richiesta', result);
+            alert('Errore durante la richiesta');
+        })
+}
