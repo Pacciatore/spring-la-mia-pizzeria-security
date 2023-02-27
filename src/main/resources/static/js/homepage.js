3c4b3d4baea46212207729d4fbda820896013d27
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-27 15:10:51 +0100

    implementazione visualizzazione delle pizze, senza ingredienti

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -1,1 +1,29 @@
 console.log('JS OK!');
+
+pizzasList();
+
+function pizzasList() {
+    axios.get('http://localhost:8080/api/pizzas')
+        .then((result) => {
+
+            const pizzasList = result.data;
+            console.log('Richiesta ok', pizzasList);
+
+            document.querySelector('#pizzas_table').innerHTML = '';
+
+            pizzasList.forEach(pizza => {
+                document.querySelector('#pizzas_table').innerHTML += `
+                <tr>
+                    <td><a href="#"> ${pizza.id} </a></td>
+                    <td>${pizza.name}</td>
+                    <td>${pizza.description}</td>
+                    <td>Vedere come implementare gli ingredienti!</td>
+                    <td>${pizza.price}</td>
+                </tr>`;
+            });
+
+        }).catch((result) => {
+            console.error('Errore nella richiesta', result);
+            alert('Errore durante la richiesta');
+        })
+}
