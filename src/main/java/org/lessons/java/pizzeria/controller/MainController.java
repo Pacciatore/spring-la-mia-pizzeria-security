798cb6da83c71dbd0d6a6cb10d2fe607495e77f7
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-27 15:37:03 +0100

    aggiunta rotta per lo show della pizza

--- src/main/java/org/lessons/java/pizzeria/controller/MainController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/MainController.java
@@ -3,6 +3,7 @@ package org.lessons.java.pizzeria.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +12,11 @@ public class MainController {
 	@GetMapping
 	public String index() {
 		return "front/homepage";
+	}
+
+	@GetMapping("/pizza")
+	public String show(@RequestParam(name = "pizza", required = false) String keyword) {
+		return "front/show";
 	}
 
 }
