824e8236d673ae78b8ba6bf6622742278e91a12b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-01 14:51:48 +0100

    completamento creazione pizza NO VALIDATION NO INGREDIENTS

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -4,5 +4,19 @@ console.log('JS OK!');
 function createPizza(event) {
     event.preventDefault();
 
-    console.log("Creazione pizza......");
+    const pizza = {
+        name: document.querySelector('#pizzaName').value,
+        description: document.querySelector('#pizzaDescription').value,
+        imgUrl: document.querySelector('#pizzaImgUrl').value,
+        price: document.querySelector('#pizzaPrice').value,
+    }
+
+    axios.post('http://localhost:8080/api/pizzas', pizza)
+        .then((result) => {
+            console.log("Inserimento dati di", pizza);
+            window.location.replace("http://localhost:8080");
+        }).catch((result) => {
+            console.error("Errore nell'inserimento", result);
+        })
+
 }
